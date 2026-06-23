74a9baef1a8f40dd71da50bb96df657b080a8097
Atik03 <atikshaharia03@gmail.com>
2026-06-23 16:43:30 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import { HiH1 } from "react-icons/hi2";
+
+export default function Home() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
